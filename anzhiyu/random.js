250cb17bef66bceb8eb06fc92fe69607e47424b4
lxhei1831 <xheiiyjie@gmail.com>
2024-11-19 18:55:01 +0800

    Site updated: 2024-11-19 18:55:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/19/LXHei-1/","2024/11/18/hello-world/","2024/11/19/test/","2024/11/19/未命名 1/","2024/11/19/未命名/"];function toRandomPost(){
+var posts=["2024/11/19/LXHei-1/","2024/11/19/test/","2024/11/18/hello-world/","2024/11/19/未命名 1/","2024/11/19/未命名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
