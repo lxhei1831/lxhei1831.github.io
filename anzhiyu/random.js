9d4019fe29157b2fd26c3857c1049bae245f8dea
lxhei1831 <xheiiyjie@gmail.com>
2024-11-23 08:36:35 +0800

    Site updated: 2024-11-23 08:36:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/20/网站/","2024/11/19/让大家看看我的宝贝~/"];function toRandomPost(){
+var posts=["2024/11/23/Hexo+GitHub搭建博客/","2024/11/20/网站/","2024/11/19/让大家看看我的宝贝~/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
