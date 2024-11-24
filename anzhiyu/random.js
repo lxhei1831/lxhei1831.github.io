74c50db8e46f886c83c9d7bbd89d7ee01c2a2ace
lxhei1831 <xheiiyjie@gmail.com>
2024-11-24 15:17:42 +0800

    Site updated: 2024-11-24 15:17:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/HexoGitHub搭建博客/","2024/11/24/Hexo分类和标签/","2024/11/24/图床搭建vercel加速/","2024/11/20/网上冲浪中转站/","2024/11/19/让大家看看我的宝贝~/"];function toRandomPost(){
+var posts=["2024/11/23/HexoGitHub搭建博客/","2024/11/24/Hexo分类和标签/","2024/11/24/图床搭建vercel加速/","2024/11/19/让大家看看我的宝贝~/","2024/11/20/网上冲浪中转站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
