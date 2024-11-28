2e15e09baf276c99e88f7eb9da2cbf09b1c6302e
lxhei1831 <xheiiyjie@gmail.com>
2024-11-28 08:43:00 +0800

    Site updated: 2024-11-28 08:42:59

--- js/alert.js
+++ js/alert.js
@@ -1,6 +1,11 @@
 Swal.fire({
     title: "这里是李小黑的博客",
-    html: "由于本站及图库均搭建在外网<br>最好使用<b><font color=\"#ffb3e6\">魔法</font></b>浏览本网站哦",
-    icon: "question",
-    allowOutsideClick: false
+    html: "由于本站及图库均搭建在外网<br>最好使用<b><font color=\"＃1E90FF\">魔法</font></b>浏览本网站哦",
+    icon: "",
+    allowOutsideClick: true,
+    showConfirmButton: false,
+    confirmButtonText: "好的",
+    imageUrl: "https://cdn.jsdelivr.net/gh/lxhei1831/blog_img@main/X2Twitter.com_EwmI-j3VEAMGDJ3_gif-ezgif.com-video-to-gif-converter%20(1).gif",
+    imageWidth: 160,
+    imageHeight: 160,
 });
